78a6eea9bc2bcfbb7f305948ce6f67a5bf82ed64
Steve Wiseman <swisemanapps@gmail.com>
2018-03-26 22:53:44 -0400

    Completed 'regex validate pin code'

---
+++ regex_validate_pin_code.py
@@ -0,0 +1,10 @@
+# ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6
+# digits. If the function is passed a valid PIN string, return true, else return false.
+
+import re
+
+def validate_pin(pin):
+    if re.search("^\d{4}$|^\d{6}$", pin):
+        return True
+    else:
+        return False
